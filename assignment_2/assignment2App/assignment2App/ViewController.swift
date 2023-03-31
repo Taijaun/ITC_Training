a22afe295cb138f7ed44b031e7f55af8a4214126
taijaunpitt <Taijaun.Pitt@informationtechconsultants.co.uk>
2023-03-31 10:21:06 +0100

    Added assignment2 app

---
+++ assignment_2/assignment2App/assignment2App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  assignment2App
+//
+//  Created by Tai Pitt on 31/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelEnterDetails: UILabel!
+    @IBOutlet weak var textFieldUsername: UITextField!
+    @IBOutlet weak var tectFieldPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        labelTitle.text = "ITC"
+        labelEnterDetails.text = "Please enter your login details"
+        
+    }
+
+
+}
+
