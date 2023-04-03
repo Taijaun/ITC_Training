b0fa330804006fad18202d0f89405d9294f17c99
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-04-03 09:53:59 +0100

    Updated assignment3 signup and reset password viewcontrollers

--- assignment_3/assignment_3/assignment_3/ResetPasswordViewController.swift
+++ assignment_3/assignment_3/assignment_3/ResetPasswordViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 class ResetPasswordViewController: UIViewController {
     
     @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var labelHelpInfo: UILabel!
+    @IBOutlet weak var buttonSend: UIButton!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         labelTitle.text = "Reset password"
+        textFieldEmail.placeholder = "E-mail"
+        labelHelpInfo.text = "Enter your email and we will send you a password reset link"
+        
+        buttonSend.setTitle("Send", for: .normal)
     }
     
 
--- assignment_3/assignment_3/assignment_3/SignUpViewController.swift
+++ assignment_3/assignment_3/assignment_3/SignUpViewController.swift
@@ -10,12 +10,24 @@ import UIKit
 class SignUpViewController: UIViewController {
     
     @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var textFieldUsername: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    @IBOutlet weak var buttonSignUp: UIButton!
+    @IBOutlet weak var switchAgreement: UISwitch!
+    @IBOutlet weak var labelAgreement: UILabel!
+    
+    
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         labelTitle.text = "Sign up now"
+        textFieldUsername.placeholder = "E-mail"
+        textFieldPassword.placeholder = "Password"
+        switchAgreement.isOn = false
+        labelAgreement.text = "I agree to the terms and conditions"
+        buttonSignUp.setTitle("Sign up", for: .normal)
 
         
     }
