f853c4e331799840a4559bfbd6e8ba1511e7ba9b
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-04-04 09:39:55 +0100

    Added constraints for resetpassword and sign up screens

--- assignment_4/assignment_4/assignment_4/ResetPasswordViewController.swift
+++ assignment_4/assignment_4/assignment_4/ResetPasswordViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class ResetPasswordViewController: UIViewController {
+    
+    
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var buttonSend: UIButton!
+    @IBOutlet weak var labelInfo: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +21,14 @@ class ResetPasswordViewController: UIViewController {
         // NavBar setup
         self.navigationItem.title = "Reset Password"
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        textFieldEmail.backgroundColor = .clear
+        textFieldEmail.borderStyle = .none
+        textFieldEmail.placeholder = "E-mail"
+        
+        buttonSend.setTitle("Send", for: .normal)
+        
+        labelInfo.text = "Enter your email and we will send you a password reset link"
     }
     
 
--- assignment_4/assignment_4/assignment_4/SignUpViewController.swift
+++ assignment_4/assignment_4/assignment_4/SignUpViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    @IBOutlet weak var textFieldConfirmPassword: UITextField!
+    @IBOutlet weak var labelAgreement: UILabel!
+    @IBOutlet weak var buttonSignUp: UIButton!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +23,25 @@ class SignUpViewController: UIViewController {
         // NavBar setup
         self.navigationItem.title = "Sign Up"
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // TextFields
+        textFieldEmail.placeholder = "E-mail"
+        textFieldEmail.backgroundColor = .clear
+        textFieldEmail.borderStyle = .none
+        
+        textFieldPassword.placeholder = "Password"
+        textFieldPassword.backgroundColor = .clear
+        textFieldPassword.borderStyle = .none
+        
+        textFieldConfirmPassword.placeholder = "Confirm Password"
+        textFieldConfirmPassword.backgroundColor = .clear
+        textFieldConfirmPassword.borderStyle = .none
+        
+        
+        labelAgreement.text = "By clicking sign up, you agree to the terms and conditions"
+        buttonSignUp.setTitle("Sign Up", for: .normal)
+        
+        
 
         
     }
