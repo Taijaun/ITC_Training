3d8b372a05d75bde3cda594803adfa3bbcc5b180
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-04-05 12:54:59 +0100

    added second section for collection view

--- assignment_4/assignment_4/assignment_4/View Controllers/ResultViewController.swift
+++ assignment_4/assignment_4/assignment_4/View Controllers/ResultViewController.swift
@@ -13,6 +13,7 @@ class ResultViewController: UIViewController {
     @IBOutlet weak var resultCollectionView: UICollectionView!
     
     let imageArray = ["trash", "calendar", "doc", "link", "person", "person.fill"]
+    let image2Array = ["space", "option", "alt", "control"]
     let numberTitles = [1, 2, 3, 4, 5, 6]
     
     
@@ -28,14 +29,30 @@ class ResultViewController: UIViewController {
 
 extension ResultViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageArray.count
+        if section == 0 {
+            return imageArray.count
+        } else {
+            return image2Array.count
+        }
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "resultCell", for: indexPath) as? ResultCollectionViewCell
-        cell?.imageViewResultCell.image = UIImage(systemName: imageArray[indexPath.item])
-        cell?.labelResultCell.text = "\(numberTitles[indexPath.item])"
-        return cell!
+        if indexPath.section == 0 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "resultCell", for: indexPath) as? ResultCollectionViewCell
+            cell?.imageViewResultCell.image = UIImage(systemName: imageArray[indexPath.item])
+            cell?.labelResultCell.text = "\(numberTitles[indexPath.item])"
+            return cell!
+        } else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "resultCell", for: indexPath) as? ResultCollectionViewCell
+            cell?.imageViewResultCell.image = UIImage(systemName: image2Array[indexPath.item])
+            cell?.labelResultCell.text = "\(numberTitles[indexPath.item])"
+            return cell!
+        }
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        2
     }
     
 }
@@ -47,8 +64,14 @@ extension ResultViewController: UICollectionViewDelegate {
         let bigImageViewController = self.storyboard?.instantiateViewController(withIdentifier: "BigImageViewController") as! BigImageViewController
         
         // set the properties for the bigImageView controller based on the info from the resultVC
-        let imageName = imageArray[indexPath.item]
-        bigImageViewController.imageName = imageName
+        if indexPath.section == 0 {
+            let imageName = imageArray[indexPath.item]
+            bigImageViewController.imageName = imageName
+        } else {
+            let imageName = image2Array[indexPath.item]
+            bigImageViewController.imageName = imageName
+        }
+        
         
         self.navigationController?.pushViewController(bigImageViewController, animated: true)
         
