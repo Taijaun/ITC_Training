f58694f2841366405ff5d8b7b84e9aff20ad3d6b
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-04-13 11:36:44 +0100

    Finished ui from detailsPage

--- assignment_9/assignment_9/assignment_9/Views/DetailViewController.swift
+++ assignment_9/assignment_9/assignment_9/Views/DetailViewController.swift
@@ -12,13 +12,37 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var labelFruitName: UILabel!
     
+    @IBOutlet weak var labelFamilyName: UILabel!
+    
+    @IBOutlet weak var labelOrderName: UILabel!
+    
+    @IBOutlet weak var labelGenusOrder: UILabel!
+    
+    @IBOutlet weak var labelCarbs: UILabel!
+    
+    @IBOutlet weak var labelProtein: UILabel!
+    
+    @IBOutlet weak var labelFat: UILabel!
+    
+    @IBOutlet weak var labelCalories: UILabel!
+    
+    @IBOutlet weak var labelSugar: UILabel!
     
     var fruit: Fruit?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard let fruit = fruit else {return}
 
-        labelFruitName.text = fruit?.name
+        labelFruitName.text = fruit.name
+        labelOrderName.text = "Order: \(fruit.order)"
+        labelGenusOrder.text = "Genus: \(fruit.genus)"
+        labelCarbs.text = "Carbs: \(fruit.nutritions.carbohydrates)"
+        labelProtein.text = "Protein: \(fruit.nutritions.protein)"
+        labelFat.text = "Fat: \(fruit.nutritions.fat)"
+        labelCalories.text = "Calories: \(fruit.nutritions.calories)"
+        labelSugar.text = "Sugar: \(fruit.nutritions.sugar)"
         
     }
     
