360c519fdbc93a2538d1c83cd80024399d3d8cea
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-04-19 15:07:05 +0100

    Initial commit

---
+++ MVVM_Assignment/MVVM_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_Assignment
+//
+//  Created by Taijaun Pitt on 19/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
