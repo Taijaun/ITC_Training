a60c27c9fa4f0ebbc044ca9be4135b4cd26380c8
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-05-10 10:35:32 +0100

    MVVM structure

--- MapKitWithAPI/MapKitWithAPI/ContentView.swift
+++ MapKitWithAPI/MapKitWithAPI/ContentView.swift
@@ -11,11 +11,12 @@ struct ContentView: View {
     var body: some View {
         VStack {
             
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
