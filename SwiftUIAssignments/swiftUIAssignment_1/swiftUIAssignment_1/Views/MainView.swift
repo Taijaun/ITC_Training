f22ade3c9c61ef949bcb313f6690d86d34363ff6
Taijaun Pitt <taijaun@hotmail.co.uk>
2023-05-12 10:18:41 +0100

    Updated ITC_Pokemon, to be MVVM-C instead of MVVM

---
+++ SwiftUIAssignments/swiftUIAssignment_1/swiftUIAssignment_1/Views/MainView.swift
@@ -0,0 +1,22 @@
+//
+//  MainView.swift
+//  swiftUIAssignment_1
+//
+//  Created by Taijaun Pitt on 12/05/2023.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
